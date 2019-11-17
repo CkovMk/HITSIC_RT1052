5872925e25b7e353e2c05d529f73b507d5d13544
Chekhov <maqikewangyi@163.com>
2019-11-17 23:03:15 +0800

    【删除】Debug

--- HITSIC_RT1052_MCUX/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -larm_cortexM7lfdp_math -larm_cortexM7lfsp_math -ltensorflow-lite -lAMCLIB -lGDFLIB -lGFLIB -lGMCLIB -lMLIB
-
